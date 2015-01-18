b819ec0d93edf7f21742f88450779e3512abd63c
Danijar Hafner <mail@danijar.org>
2015-01-18 14:21:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Web Audio Clock
+===============
+
+This small project is a webpage thatplays a clock sound every second. Every ten
+seconds, a different sound is played. The elapsed seconds are displayed on the
+page.
+
+Attribution
+-----------
+
+The [tick][1] and the [tock][2] are from SoundBible.
+
+[1]: http://soundbible.com/2044-Tick.html
+[2]: http://soundbible.com/783-Click.html
---
+++ script.js
@@ -0,0 +1,32 @@
+// Global constants
+var tick = new Audio('tick.mp3');
+var tock = new Audio('tock.mp3');
+var element = document.getElementById('elapsed');
+
+// Global variables
+var interval;
+var seconds = 0;
+
+// Create interval for the clock
+function startClock() {
+	interval = setInterval(function() {
+		if (seconds % 10 == 0)
+			tock.play();
+		else
+			tick.play();
+		seconds++;
+		elapsed.innerHTML = seconds.toString();
+	}, 1000);
+}
+
+// Remove the current lock interval
+function stopClock() {
+	clearInterval(interval);
+}
+
+// Start initial clock once
+startClock();
+
+// Pause clock when tab loses focus
+window.onfocus = function() { startClock() };
+window.onblur = function() { stopClock() };
